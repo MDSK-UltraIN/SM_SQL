86c9adc1b5c45d00897379275dae9beeee40d30f
MDSK-UltraIN <oacgodkl21@gmail.com>
2017-09-03 20:09:52 +0800

    update

--- SM_Detect_INSERT_RPi.py
+++ SM_Detect_INSERT_RPi.py
@@ -7,12 +7,17 @@ password = 'PASSWORD'
 database = 'SM'
 
 
-PERSONGROUPID = 'Person'+repr(random.randint(0,999)) #PersonGroupID在這裡
+
 
 cnsn = pyodbc.connect('DSN=%s;UID=%s;PWD=%s;DATABASE=%s;'%(dsn,user,password,database))
 cursor = cnsn.cursor()
+
+
+Person_ID = 'p38amucp8um4' #ID從這裡改
+Person_Name = 'Person名字'#name從這裡改
+
 #SQLCommand = "INSERT INTO [dbo].[test]([faceID],[userName],[dateregis]) VALUES ('test888-777-1859"+repr(random.randint(0,999))+"',"+repr(Namevartest)+",CONVERT(DATETIME,SYSDATETIMEOFFSET() AT TIME ZONE 'China Standard Time'))"
-cursor.execute("INSERT INTO [dbo].[detect]([PersonGroupID],[detecttime]) VALUES ("+repr(PERSONGROUPID)+",CONVERT(DATETIME,SYSDATETIMEOFFSET() AT TIME ZONE 'China Standard Time'))"
+cursor.execute("INSERT INTO [dbo].[detect]([PersonID],[Person],[detecttime]) VALUES ('"+Person_ID+"','"+Person_Name+"',CONVERT(DATETIME,SYSDATETIMEOFFSET() AT TIME ZONE 'China Standard Time'))"
 )
 cnsn.commit() #hen IMPORTANT
 """cursor.execute("select * from [dbo].[test]")
